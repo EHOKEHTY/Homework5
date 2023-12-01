e34219078281ceb821c6fded13b11bab56c16349
Женя Остапенко <jenya2003a@gmail.com>
2023-12-01 15:51:33 +0200

    Добавьте файлы проекта.

---
+++ Homework5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
